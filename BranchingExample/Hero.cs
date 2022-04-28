1d0a717eaa70a7091817b37d35f3440ef1c5c7a3
Tommy Waalkes <Tommy@grandcircus.co>
2022-04-28 10:02:15 -0400

    adding everything else

---
+++ BranchingExample/Hero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingExample
+{
+    class Hero
+    {
+        public int HP { get; set; }
+        public int Attack { get; set; }
+
+        public Hero(int HP, int Attack)
+        {
+            this.HP = HP;
+            this.Attack = Attack;
+        }
+
+    }
+}
---
+++ BranchingExample/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BranchingExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Hero h = new Hero(10, 20);
+            Console.WriteLine("Hero Attack: "+h.Attack);
+        }
+             
+    }
+}
